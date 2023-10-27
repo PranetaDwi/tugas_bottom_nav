282705e7b52f7228cb95086518faceea64c8075b
PranetaDwi <pranetadwiindarti2004@mail.ugm.ac.id>
2023-10-28 06:49:09 +0700

    fiks features

--- app/src/main/java/com/neta/tugas_bottom_nav/MainActivity.kt
+++ app/src/main/java/com/neta/tugas_bottom_nav/MainActivity.kt
@@ -2,10 +2,35 @@ package com.neta.tugas_bottom_nav
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.neta.tugas_bottom_nav.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Home())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.nav_home -> replaceFragment(Home())
+                R.id.nav_profile -> replaceFragment(Profile())
+                R.id.nav_bioskop -> replaceFragment(Bioskop())
+
+                else -> {}
+            }
+            true
+        }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }
